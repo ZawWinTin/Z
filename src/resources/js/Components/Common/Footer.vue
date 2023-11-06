42c69ec521b74f6f7b7b29c9b3e515209caca578
Zaw Win Tin <zawwintin21@gmail.com>
2023-11-06 15:24:48 +0630

    Update MainLayout

--- src/resources/js/Components/Common/Footer.vue
+++ src/resources/js/Components/Common/Footer.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-    <div class="bg-gray-800 py-1">
+    <div class="bg-gray-800 py-1 mt-auto">
         <p class="text-center text-white">This is Footer.</p>
     </div>
 </template>
--- src/resources/js/Layouts/MainLayout.vue
+++ src/resources/js/Layouts/MainLayout.vue
@@ -2,13 +2,15 @@
 import { Link } from '@inertiajs/vue3';
 import Header from '@components/Common/Header.vue';
 import Footer from '@components/Common/Footer.vue';
+import Cursor from '@components/Common/Cursor.vue';
 </script>
 <template>
-    <main class="scroll-smooth duration-300 ease-in-out selection:bg-primary selection:text-white dark:bg-black">
+    <main class="scroll-smooth duration-300 ease-in-out selection:bg-primary/80 selection:text-white dark:bg-black min-h-screen flex flex-col">
         <Header></Header>
-        <article class="min-h-[89.3vh]">
+        <article>
             <slot />
         </article>
+        <!-- <Cursor /> -->
         <Footer></Footer>
     </main>
 </template>
