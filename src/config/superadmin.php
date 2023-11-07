deeff67b04fdba85de306919e03eceb3837c127b
Zaw Win Tin <zawwintin21@gmail.com>
2023-11-07 16:50:08 +0630

    Add UserSeeder

---
+++ src/config/superadmin.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'auth' => [
+        'name' => env('SUPER_ADMIN_NAME', 'Super Admin'),
+        'email' => env('SUPER_ADMIN_EMAIL', 'admin@admin.com'),
+        'password' => env('SUPER_ADMIN_PASSWORD', 'password'),
+    ],
+];
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $seeders = [
+            UserSeeder::class,
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($seeders as $seeder) {
+            // Prevent already executed seeder
+            if (DB::table('execute_seeds')->where('name', $seeder)->doesntExist()) {
+                if (!App::environment('testing')) {
+                    echo __CLASS__ . '::' . __FUNCTION__ . '(' . __LINE__ . ')[' . getmypid() . "][DEBUG_SEED][exec:{$seeder}]\n";
+                }
+                $this->call($seeder);
+                DB::table('execute_seeds')->insert(['name' => $seeder]);
+            } else {
+                if (!App::environment('testing')) {
+                    echo __CLASS__ . '::' . __FUNCTION__ . '(' . __LINE__ . ')[' . getmypid() . "][DEBUG_SEED][skip:{$seeder}]\n";
+                }
+            }
+        }
     }
 }
