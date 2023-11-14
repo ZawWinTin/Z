c8fd0693aeaa82083796bfd5dd43bb384783f8ec
Zaw Win Tin <zawwintin21@gmail.com>
2023-11-14 14:15:22 +0630

    Optimize Code

--- src/resources/js/Components/ApplicationLogo.vue
+++ src/resources/js/Components/ApplicationLogo.vue
@@ -2,7 +2,7 @@
     <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        class="tw-select-none tw-stroke-black tw-text-2xl tw-font-bold tw-text-black dark:tw-stroke-white dark:tw-text-white tw-duration-300 tw-ease-in-out"
+        class="tw-select-none tw-stroke-black tw-text-2xl tw-font-bold tw-text-black tw-duration-300 tw-ease-in-out dark:tw-stroke-white dark:tw-text-white"
     >
         <text
             x="3"
