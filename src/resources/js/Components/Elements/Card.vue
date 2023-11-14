c9da835bae2ab0c4f6928291fb52075857b6a45e
Zaw Win Tin <zawwintin21@gmail.com>
2023-11-14 08:16:35 +0630

    Implement Card night mode smooth animation

--- src/resources/js/Components/Elements/Card.vue
+++ src/resources/js/Components/Elements/Card.vue
@@ -46,7 +46,7 @@ let loadImage = () => {
                 class="tw-relative tw-mt-[-3.3rem] tw-transition tw-duration-500 group-hover:tw-translate-y-[-5.5rem]"
             >
                 <div
-                    class="card-background w-full tw-absolute tw-rounded-2xl tw-bg-white dark:tw-bg-gray-800"
+                    class="card-background w-full tw-absolute tw-rounded-2xl tw-bg-white dark:tw-bg-gray-800 tw-duration-300 tw-ease-in-out"
                 ></div>
                 <div class="tw-relative tw-flex tw-flex-col tw-gap-2 tw-p-5">
                     <p
