81d3756f7d5d23d627a463303744c433f55d98f6
Zaw Win Tin <zawwintin21@gmail.com>
2023-11-28 13:24:44 +0630

    Update Toast Design

--- src/resources/js/Components/UI/Toast.vue
+++ src/resources/js/Components/UI/Toast.vue
@@ -5,19 +5,19 @@ import Toast from 'primevue/toast';
     <Toast
         :pt="{
             root: {
-                class: ['tw-w-96', 'tw-opacity-90'],
+                class: ['tw-w-96'],
             },
             container: ({ props }) => ({
                 class: [
-                    'tw-my-4 tw-rounded-md tw-w-full',
+                    'tw-my-4 tw-rounded-md tw-w-full tw-bg-opacity-70 dark:tw-bg-opacity-30 tw-transition tw-duration-300',
                     {
-                        'tw-bg-blue-100 tw-border-solid tw-border-0 tw-border-l-4 tw-border-blue-500 tw-text-blue-700':
+                        'tw-bg-blue-100 dark:tw-bg-blue-800 tw-border-solid tw-border-0 tw-border-l-4 tw-border-blue-500 tw-text-blue-500':
                             props.message.severity == 'info',
-                        'tw-bg-green-100 tw-border-solid tw-border-0 tw-border-l-4 tw-border-green-500 tw-text-green-700':
+                        'tw-bg-green-100 dark:tw-bg-green-800 tw-border-solid tw-border-0 tw-border-l-4 tw-border-green-500 tw-text-green-500':
                             props.message.severity == 'success',
-                        'tw-bg-orange-100 tw-border-solid tw-border-0 tw-border-l-4 tw-border-orange-500 tw-text-orange-700':
+                        'tw-bg-orange-100 dark:tw-bg-orange-800 tw-border-solid tw-border-0 tw-border-l-4 tw-border-orange-500 tw-text-orange-500':
                             props.message.severity == 'warn',
-                        'tw-bg-red-100 tw-border-solid tw-border-0 tw-border-l-4 tw-border-red-500 tw-text-red-700':
+                        'tw-bg-red-100 dark:tw-bg-red-800 tw-border-solid tw-border-0 tw-border-l-4 tw-border-red-500 tw-text-red-500':
                             props.message.severity == 'error',
                     },
                 ],
@@ -26,7 +26,7 @@ import Toast from 'primevue/toast';
             icon: {
                 class: ['tw-w-6 tw-h-6', 'tw-text-lg tw-mr-2'],
             },
-            text: 'tw-text-base tw-font-normal tw-flex tw-flex-col tw-flex-1 grow shrink tw-ml-4',
+            text: 'tw-text-base tw-font-normal tw-flex tw-flex-col tw-flex-1 tw-grow tw-shrink tw-ml-4',
             summary: 'tw-font-bold tw-block',
             detail: 'tw-mt-1 tw-block',
             closebutton: {
@@ -39,7 +39,7 @@ import Toast from 'primevue/toast';
             },
             transition: {
                 enterFromClass:
-                    'tw-opacity-0 tw-translate-x-0 tw-translate-y-2/4 translate-z-0',
+                    'tw-opacity-0 tw-translate-x-0 tw-translate-y-2/4 tw-translate-z-0',
                 enterActiveClass:
                     'tw-transition-transform tw-transition-opacity tw-duration-300',
                 leaveFromClass: 'tw-max-h-40',
