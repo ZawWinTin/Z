ed94109bb2d1112ee6eb055210229cfa950ccd29
Zaw Win Tin <zawwintin21@gmail.com>
2023-12-28 10:26:07 +0630

    Run Pint

--- src/tests/Feature/Auth/EmailVerificationTest.php
+++ src/tests/Feature/Auth/EmailVerificationTest.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\URL;
+use App\Providers\RouteServiceProvider;
 
 test('email verification screen can be rendered', function () {
     $user = User::factory()->create([
@@ -33,7 +33,7 @@ test('email can be verified', function () {
 
     Event::assertDispatched(Verified::class);
     expect($user->fresh()->hasVerifiedEmail())->toBeTrue();
-    $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
+    $response->assertRedirect(RouteServiceProvider::HOME . '?verified=1');
 });
 
 test('email is not verified with invalid hash', function () {
